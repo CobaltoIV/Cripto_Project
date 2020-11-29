87fb31972d94b0a70bf038a63ceb126a3c2e84f1
CobaltoIV <62718484+CobaltoIV@users.noreply.github.com>
2020-11-29 13:01:10 +0000

    Setup
    Created keys and cerificates for CA and Clients

---
+++ CSC/CMakeFiles/CA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CA"
+  "CA.pdb"
+  "CMakeFiles/CA.dir/CA.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CSC/Tests/test.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{   
+    string command;
+    cout<<"Type something:";
+
+    getline(cin, command);
+
+    cout<<"Your command:"<<command<<"\n";
+}
