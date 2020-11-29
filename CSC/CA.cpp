08c42efcfb43eea26d245cb06ebc38bfa2539261
CobaltoIV <62718484+CobaltoIV@users.noreply.github.com>
2020-11-29 22:00:42 +0000

    server certificate and keys done

--- CSC/CA.cpp
+++ CSC/CA.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 	//Handling input parameters
 	for (int i = 0; i < argc; ++i)
 	{
-		if (strcmp(argv[i], "-c") == 0) 
+		if (strcmp(argv[i], "-c") == 0)
 			clientcount = atoi(argv[++i]);
 
 		//if (strcmp(argv[i], "-o") == 0) //verbose mode, using /dev/null to suppress console output
@@ -62,15 +62,15 @@ int main(int argc, char *argv[])
     cout << "Creating SEAL keys\n"
 		 << endl;
 	fstream fs("DBpublic_key.txt", fstream::binary | fstream::out);
-	public_key.save(fs); 
+	public_key.save(fs);
 	fs.close();
 	fs.open("DBprivate_key.txt", fstream::binary | fstream::out);
 	private_key.save(fs);
 	fs.close();
-	
+
 
 	Encryptor encryptor(context, public_key);
-	
+
     // Filesystem creation
 	cout << "Create Database filesystem" << endl;
 
@@ -86,169 +86,79 @@ int main(int argc, char *argv[])
 
     cout << "ClientApp created"<< endl;
     system("mv DBpublic_key.txt Admin");
-    /*
-	//generating a 32 byte (256 bit) symmetric key
-	system("openssl rand 32 > /tmp/symmetric.key");
+	system("mv DBprivate_key.txt Admin");
 
-	//encrypting the original homormophic private key
-	sprintf(systemcall, "openssl aes-256-cbc -a -salt -in DBprivkey.txt -out Admin/DBprivkey.txt.enc -pass file:/tmp/symmetric.key%s", cmdout);
-	system(systemcall);
-    // deleting original
-	system("rm -r DBprivkey.txt");
-	
-    cout << "Private key sucessfully encrypted" << endl;
-    */
 	//Generate a root CA certificate and private key
 	cout << "Generating CA cert" << endl;
 	sprintf(systemcall, "openssl req -x509 -sha512 -nodes -days 365 -newkey rsa:2048 -keyout CAprivate_key.key -out CAcert.crt -subj \"/C=PT/ST=Lisbon/L=Lisbon/O=Cripto/OU=CSC-Project/CN=CA\"%s && mv CAprivate_key.key CAcert.crt Admin", cmdout);
 	system(systemcall);
     cout << "Operation successful"<<endl;
 
-	//Install the root certificate and tally's own certificate and private key in the tally official app
 
-	
+
+	/*
 	//Signing the database public key with the CA private key
 	cout << "Signing the database public key with the CA cert" << endl;
 	sprintf(systemcall, "openssl dgst -sha512 -sign Admin/CAprivate_key.key -out /tmp/sign.sha512 Admin/DBpublic_key.txt%s",cmdout);
 	system(systemcall);
 	sprintf(systemcall, "openssl base64 -in /tmp/sign.sha512 -out Admin/DBpublic_key_signed.txt%s", cmdout);
 	system(systemcall);
-	cout << "Creating client key pairs and signing them with CA.cert"
-		 << endl;
-	
-	//TODO Create certificates and keys for server
+	*/
 
+	// generate server key pair
+	// installing the root CA cert
+	sprintf(systemcall, "cp Admin/CAcert.crt Server");
+	system(systemcall);
+	cout<<"Generating Server Private Key and certificate request"<<endl;
+	sprintf(systemcall, "cd Server && openssl genrsa -out server_pk.key 1024%s && openssl req -new -key server_pk.key -out server-cert.csr -subj \"/C=PT/ST=Lisbon/L=Lisbon/O=Cripto/OU=CSC-Project/CN=Server\" %s", cmdout,cmdout);
+	system(systemcall);
+	cout<<"Signing Server Certificate with CA Private Key"<<endl;
+	//signing the server  key with the CA private key
+	sprintf(systemcall, "cd Server && openssl x509 -req -in server-cert.csr -out server-cert.crt -sha1 -CA CAcert.crt -CAkey ../Admin/CAprivate_key.key -CAcreateserial -days 3650 %s", cmdout);
+	system(systemcall);
+
+
+	cout << "Creating client key pairs and signing them with CA.cert" << endl;
 	for (int i = 1; i <= clientcount; ++i)
 	{
 		//creating the voter directory
 		sprintf(systemcall, "cd ClientApp && mkdir Client%d", i);
 		system(systemcall);
 
-		//installing the election public key and respective signature
-		sprintf(systemcall, "cp Admin/DBpublic_key_signed.txt /DBpublic_key.txt ClientApp/Client%d", i);
+		//installing the Dtabase Key Server and CA certificates
+		sprintf(systemcall, "cp Admin/DBpublic_key.txt Admin/DBprivate_key.txt Server/server-cert.crt Admin/CAcert.crt  ClientApp/Client%d", i);
 		system(systemcall);
 
-		//installing the root CA cert
-		sprintf(systemcall, "cp Admin/CAcert.crt ClientApp/Client%d", i);
-		system(systemcall);
-
-		//generating clients key pairs 
+		//generating clients key pairs
 		sprintf(systemcall, "cd Admin && openssl genrsa -out c%dpk.key 1024%s && openssl req -new -key c%dpk.key -out c%d-cert.csr -subj \"/C=PT/ST=Lisbon/L=Lisbon/O=Cripto/OU=CSC-Project/CN=c%d\" %s", i,cmdout, i, i, i,cmdout);
 		system(systemcall);
 
+		/*
 		//signing the client private key with the CA private key
 		sprintf(systemcall, "cd Admin && openssl dgst -sha512 -sign CAprivate_key.key -out /tmp/sign.sha512 c%dpk.key %s", i,cmdout);
 		system(systemcall);
 		sprintf(systemcall, "cd Admin && openssl base64 -in /tmp/sign.sha512 -out c%dpk_signed.txt %s", i,cmdout);
 		system(systemcall);
+		*/
 
-		//installing private key and its signture into Client dir
-		sprintf(systemcall, "cd Admin && mv c%dpk_signed.txt c%dpk.key ../ClientApp/Client%d", i, i, i);
+		//installing private key into Client dir
+		sprintf(systemcall, "cd Admin && mv c%dpk.key ../ClientApp/Client%d", i, i);
 		system(systemcall);
-
 		//converting certificate request into actual certificate and signing it with CA private key
 		sprintf(systemcall, "cd Admin && openssl x509 -req -in c%d-cert.csr -out c%d-cert.crt -sha1 -CA CAcert.crt -CAkey CAprivate_key.key -CAcreateserial -days 3650 %s", i, i, cmdout);
 		system(systemcall);
 		cout << "Client Certificate created sucessfully" << endl;
+		/*
 		sprintf(systemcall, "cd Admin && openssl dgst -sha512 -sign CAprivate_key.key -out /tmp/sign.sha512 c%d-cert.crt %s", i,cmdout);
 		system(systemcall);
 		sprintf(systemcall, "cd Admin && openssl base64 -in /tmp/sign.sha512 -out c%d-cert_signed.txt %s", i,cmdout);
 		system(systemcall);
-
-		//installing voter certificate
-		sprintf(systemcall, "cd Admin && mv c%d-cert_signed.txt c%d-cert.crt ../ClientApp/Client%d", i, i, i);
+		*/
+		//installing client certificate
+		sprintf(systemcall, "cd Admin && mv c%d-cert.crt ../ClientApp/Client%d", i, i);
 		system(systemcall);
 	}
 
-	/*
-	//generating a bad key to be used by an ill intensioned voter
-	sprintf(systemcall, "cd CA && openssl genrsa -out hackerpk.key 1024%s",cmdout);
-	system(systemcall);
-
-	//we've usec the symmetric key to encrypt the election private key, it will now be split
-	//first we need to load it from the file so we can split
-	memset(data, 0, sizeof(data));
-	cout << "Splitting symetric key among trustees\n"
-		 << endl;
-	fstream fb;
-	fb.open("/tmp/sym_key.key", fstream::in | fstream::binary);
-	if (fb.is_open())
-	{
-		//file is exactly 32 bytes, as expected...
-		fb.read(stringex, 32);
-		memcpy(&data, &stringex, 32);
-		fb.close();
-	}
-	else
-		cout << "Unable to open file";
-
-	//splitting the secret
-	sss_create_shares(shares, data, trusteecount, 30);
-
-	for (int i = 1; i <= trusteecount; ++i)
-	{
-		//saving the share in a file
-		sprintf(stringex, "trustees/trustee%d", i);
-		fstream file(stringex, fstream::out | fstream::binary);
-		if (file.is_open())
-		{
-			file.write((char *)shares[i - 1], 113);
-			file.close();
-
-			//signing the share
-			sprintf(systemcall, "openssl dgst -sha256 -sign CA/CAprivKey.key -out /tmp/sign.sha256 trustees/trustee%d%s", i,cmdout);
-			system(systemcall);
-			sprintf(systemcall, "openssl base64 -in /tmp/sign.sha256 -out trustees/trustee%dsgn", i);
-			system(systemcall);
-		}
-		else
-			cout << "Unable to open file";
-	}
-
-	//program asks the administrator for the weights of the voters
-	string line;
-    int d;
-	Ciphertext weight;
-	for (int i = 1; i <= clientcount; ++i)
-	{
-		sprintf(stringex, "Enter weight of Voter %d:\n", i);
-        cout << stringex << endl;
-        while (getline(cin, line))
-        {   
-            stringstream ss(line);
-            if (ss >> d)
-            {
-                if (ss.eof())
-                {   // Success
-                    if (d >= 1 && d <= 10)
-					{
-						//first we need to encode the weight...
-						Plaintext x_plain(to_string(d));
-
-						//so we can then encrypt it
-						encryptor.encrypt(x_plain, weight);
-
-						//saving the file
-						sprintf(stringex, "tallyApp/weight%d.txt", i);
-						ofstream ofs(stringex, ofstream::binary);
-						weight.save(ofs);
-						ofs.close();
-
-						//and signing the weight file
-						sprintf(systemcall, "openssl dgst -sha256 -sign CA/CAprivKey.key -out /tmp/sign.sha256 tallyApp/weight%d.txt%s", i,cmdout);
-						system(systemcall);
-						sprintf(systemcall, "openssl base64 -in /tmp/sign.sha256 -out tallyApp/weight%dsgn.txt%s", i, cmdout);
-						system(systemcall);
-                    	break;						
-					}
-                }
-                std::cout << "Error! Only weights values between 1 and 10" << std::endl;
-            }
-            sprintf(stringex, "Insert valid weight for voter %d: ", i);
-            cout << stringex;
-		}	
-	}
-	cout << "Election is now ready to begin\n" << endl;
+	cout << "Database is now ready\n" << endl;
 	return 0;
-    */
 }
