c51c25e335836168b717cb969e2ba45e247042a9
CobaltoIV <62718484+CobaltoIV@users.noreply.github.com>
2020-12-01 00:11:06 +0000

    Tests folder update

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,13 +1,152 @@
 
 #include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <cstddef>
+#include <fstream>
+#include <iomanip>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <random>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <seal/seal.h>
 using namespace std;
+using namespace seal;
+
+template <typename T>
+inline void print_matrix(std::vector<T> matrix, std::size_t row_size)
+{
+    /*
+    We're not going to print every column of the matrix (there are 2048). Instead
+    print this many slots from beginning and end of the matrix.
+    */
+    std::size_t print_size = 5;
+
+    std::cout << std::endl;
+    std::cout << "    [";
+    for (std::size_t i = 0; i < print_size; i++)
+    {
+        std::cout << std::setw(3) << std::right << matrix[i] << ",";
+    }
+    std::cout << std::setw(3) << " ...,";
+    for (std::size_t i = row_size - print_size; i < row_size; i++)
+    {
+        std::cout << std::setw(3) << matrix[i] << ((i != row_size - 1) ? "," : " ]\n");
+    }
+    std::cout << "    [";
+    for (std::size_t i = row_size; i < row_size + print_size; i++)
+    {
+        std::cout << std::setw(3) << matrix[i] << ",";
+    }
+    std::cout << std::setw(3) << " ...,";
+    for (std::size_t i = 2 * row_size - print_size; i < 2 * row_size; i++)
+    {
+        std::cout << std::setw(3) << matrix[i] << ((i != 2 * row_size - 1) ? "," : " ]\n");
+    }
+    std::cout << std::endl;
+}
 
 int main(int argc, char *argv[])
-{   
+{
+    /*
     string command;
     cout<<"Type something:";
 
     getline(cin, command);
 
     cout<<"Your command:"<<command<<"\n";
+    */
+    EncryptionParameters parms(scheme_type::bfv);
+    size_t poly_modulus_degree = 8192;
+    parms.set_poly_modulus_degree(poly_modulus_degree);
+    parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
+    parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 20));
+    SEALContext context(parms);
+    KeyGenerator keygen(context);
+    PublicKey public_key;
+    keygen.create_public_key(public_key);
+    SecretKey secret_key = keygen.secret_key();
+    Encryptor encryptor(context, public_key);
+    Evaluator evaluator(context);
+    Decryptor decryptor(context, secret_key);
+    BatchEncoder batch_encoder(context);
+
+    size_t slot_count = batch_encoder.slot_count();
+    size_t row_size = slot_count / 2;
+
+    int x = 14, y = 1;
+    int n = x;
+    int digit = 0;
+
+    vector<uint64_t> pod_matrix1;
+    vector<uint64_t> pod_matrix2;
+    vector<uint64_t> pod_result;
+
+    cout << "[";
+    for (size_t i = 0; i < slot_count; i++)
+    {
+        if (n > 0)
+        {
+            digit = n % 2;
+            n = n / 2;
+        }
+        else digit = 0;
+        pod_matrix1.push_back(digit);
+        pod_matrix2.push_back(digit);
+    }
+
+    Plaintext plain_matrix1, plain_matrix2, plain_result;
+    Ciphertext encrypted_matrix1, encrypted_matrix2, encrypted_matrix3;
+    batch_encoder.encode(pod_matrix1, plain_matrix1);
+    batch_encoder.encode(pod_matrix2, plain_matrix2);
+    encryptor.encrypt(plain_matrix1, encrypted_matrix1);
+    encryptor.encrypt(plain_matrix2, encrypted_matrix2);
+    evaluator.sub(encrypted_matrix1, encrypted_matrix2, encrypted_matrix3);
+    decryptor.decrypt(encrypted_matrix3, plain_result);
+    batch_encoder.decode(plain_result, pod_result);
+    cout << "    + Result plaintext matrix ...... Correct." << endl;
+    print_matrix(pod_result, row_size);
+
+
+    Plaintext x_plain(to_string(x)), y_plain(to_string(y));
+    Ciphertext x_encrypted, y_encrypted, result_encrypted;
+    encryptor.encrypt(x_plain, x_encrypted);
+    encryptor.encrypt(y_plain, y_encrypted);
+    evaluator.add(x_encrypted, y_encrypted, result_encrypted);
+    Plaintext result;
+    decryptor.decrypt(result_encrypted, result);
+    cout << "0x" << result.to_string() << " ...... Correct." << endl;
+
+    fstream fb;
+    char stringex[50];
+    sprintf(stringex, "result.txt");
+    fb.open(stringex, fstream::binary | fstream::out);
+    result_encrypted.save(fb);
+    fb.close();
+
+    sprintf(stringex, "x.txt");
+    fb.open(stringex, fstream::binary | fstream::out);
+    x_encrypted.save(fb);
+    fb.close();
+
+    sprintf(stringex, "y.txt");
+    fb.open(stringex, fstream::binary | fstream::out);
+    y_encrypted.save(fb);
+    fb.close();
+
+    sprintf(stringex, "y.txt");
+    fb.open(stringex, fstream::binary | fstream::out);
+    y_encrypted.save(fb);
+    fb.close();
+
+    sprintf(stringex, "pod.txt");
+    fb.open(stringex, fstream::binary | fstream::out);
+    encrypted_matrix3.save(fb);
+    fb.close();
+
 }
